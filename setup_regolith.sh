f09067e5f87921959035164a0441fbf2ee4c6dd2
acr <andre.rodrigues.dev@gmail.com>
2022-10-24 13:56:03 +0100

    added rigolith setup

---
+++ setup_regolith.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -qO - https://regolith-desktop.org/regolith.key | \
+gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+https://regolith-desktop.org/release-ubuntu-jammy-amd64 jammy main" | \
+sudo tee /etc/apt/sources.list.d/regolith.list
+
+sudo apt update
+sudo apt install regolith-desktop regolith-look-* 3xrocks-focused-window-name i3xrocks-rofication i3xrocks-info i3xrocks-app-launcher i3xrocks-memory i3xrocks-battery regolith-compositor-picom-glx 
+sudo apt upgrade

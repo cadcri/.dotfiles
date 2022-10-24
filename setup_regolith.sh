bc8f4b37859b51b2bf79de7b24170bbceb3f36c8
acr <celso.rodrigues.dev@gmail.com>
2022-10-24 16:10:37 +0200

    repair setup regolith

--- setup_regolith.sh
+++ setup_regolith.sh
@@ -5,9 +5,9 @@ wget -qO - https://regolith-desktop.org/regolith.key | \
 gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
 
 echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
-https://regolith-desktop.org/release-ubuntu-jammy-amd64 jammy main" | \
+https://regolith-desktop.org/release-debian-bullseye-amd64 debian main" | \
 sudo tee /etc/apt/sources.list.d/regolith.list
 
 sudo apt update
-sudo apt install -y regolith-desktop regolith-look-* 3xrocks-focused-window-name i3xrocks-rofication i3xrocks-info i3xrocks-app-launcher i3xrocks-memory i3xrocks-battery regolith-compositor-picom-glx 
+sudo apt install -y regolith-desktop regolith-look-* i3xrocks-focused-window-name i3xrocks-rofication i3xrocks-info i3xrocks-app-launcher i3xrocks-memory i3xrocks-battery regolith-compositor-picom-glx 
 sudo apt upgrade

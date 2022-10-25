181a8b36c3500c4c1d791cfa667f1157e9c8ba40
acr <andre.rodrigues.dev@gmail.com>
2022-10-25 05:33:05 +0100

    a

--- setup.sh
+++ setup.sh
@@ -2,10 +2,6 @@
 
 # SYSTEM CONFIG
 
-# skip grub menu
-sudo sed -i -E 's/GRUB_TIMEOUT=([[:digit:]]+)/GRUB_TIMEOUT=0/' /etc/default/grub
-sudo update-grub
-
 # enable hibernate on lid close and button press
 sudo tee <<EOF /etc/systemd/logind.conf 1> /dev/null
 [Login]
@@ -13,36 +9,30 @@ HandleLidSwitch=hibernate
 HandlePowerKey=hibernate # do nothing on regolith
 EOF
 
-#####
-
 # to not have screens asking for restart services
-export DEBIAN_FRONTEND=noninteractive
+# export DEBIAN_FRONTEND=noninteractive
 
 # install packages
-sudo apt install -y ntp bash-completion preload git stow tmux xclip curl htop rfkill build-essential unzip vlc nvim
+sudo apt install -y git stow tmux curl htop tree build-essential vlc neovim preload docker.io gimp
+sudo snap install --classic intellij-idea-community android-studio
+sudo snap install --beta steam 
+sudo snap remove firefox
 
-# setup dotfiles
-rm ~/.bashrc ~/.profile ~/.bash_logout
-mkdir ~/.config
-stow files
+# install non apt/snap apps
+ curl -L --output-dir /tmp \
+	 -o discord.deb "https://discord.com/api/download?platform=linux&format=deb" \
+	 -o minikube.deb "https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb" \
+	 -o chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+sudo apt install -y /tmp/*.deb
 
-# install neovim 
-curl -Lo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# install chrome
-curl -LO https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome-stable_current_amd64.deb
-rm ./google-chrome-stable_current_amd64.deb
-
-# install docker
-sudo apt install -y docker.io
-sudo usermod -aG docker $USER
-newgrp docker
+# setup docker and kubernetes
+sudo usermod -aG docker $USER && newgrp docker
+minikube start && minikube delete --all
 sudo systemctl disable --now docker containerd
 
-# install minikube
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-sudo apt install -y ./minikube_latest_amd64.deb
-rm ./minikube_latest_amd64.deb
+# setup dotfiles
+rm ~/.{bashrc,profile,bash_logout}
+rmdir ~/{Desktop,Documents,Downloads,Music,Pictures,Public,Templates,Videos}
+mkdir ~/{.config,desktop,documents,downloads,pictures,public,videos}
+cd ~/.dotfiles && stow files
 

dd3614a00b466d825d1f5c7ee0298e90d8351e29
acr <andre.rodrigues.dev@gmail.com>
2022-11-01 00:53:16 +0000

    a

--- setup.sh
+++ setup.sh
@@ -18,23 +18,22 @@ sudo snap install --classic intellij-idea-community android-studio code
 sudo snap install --beta steam 
 sudo snap remove firefox
 
+
+# setup docker 
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
 # install non apt/snap apps
  curl -L --output-dir /tmp \
 	 -o discord.deb "https://discord.com/api/download?platform=linux&format=deb" \
-	 -o minikube.deb "https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb" \
+	 -o docker.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.13.1-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64" \
 	 -o chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo apt install -y /tmp/*.deb
 
-wget -O gnome-shell-extension-installer "https://github.com/brunelli/gnome-shell-extension-installer/raw/master/gnome-shell-extension-installer"
-chmod +x gnome-shell-extension-installer
-sudo mv gnome-shell-extension-installer /usr/bin/
-gnome-shell-extension-installer 744
-gnome-shell-extension-installer 744
-
-# setup docker and kubernetes
-sudo usermod -aG docker $USER && newgrp docker
-minikube start && minikube delete --all
-sudo systemctl disable --now docker containerd
 
 # setup dotfiles
 rm ~/.{bashrc,profile,bash_logout}

948e599ad44e2fc5692857fbd4b21f9c772d978f
acr <andre.rodrigues.dev@gmail.com>
2022-11-02 14:50:45 +0000

    a

--- setup.sh
+++ setup.sh
@@ -2,22 +2,11 @@
 
 # SYSTEM CONFIG
 
-# enable hibernate on lid close and button press
-sudo tee <<EOF /etc/systemd/logind.conf 1> /dev/null
-[Login]
-HandleLidSwitch=hibernate
-HandlePowerKey=hibernate # do nothing on regolith
-EOF
-
-# to not have screens asking for restart services
-# export DEBIAN_FRONTEND=noninteractive
-
 # install packages
-sudo apt install -y git stow tmux curl htop tree build-essential vlc neovim preload docker.io gimp gnome-tweaks qbittorrent
+sudo apt install -y git stow tmux curl htop tree build-essential vlc neovim preload gimp gnome-tweaks qbittorrent
 sudo snap install --classic intellij-idea-community android-studio code
 sudo snap install --beta steam 
 sudo snap remove firefox
-
 
 # setup docker 
 sudo mkdir -p /etc/apt/keyrings
@@ -30,7 +19,7 @@ echo \
 # install non apt/snap apps
  curl -L --output-dir /tmp \
 	 -o discord.deb "https://discord.com/api/download?platform=linux&format=deb" \
-	 -o docker.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.13.1-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64" \
+	 -o docker.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.13.1-amd64.deb" \
 	 -o chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo apt install -y /tmp/*.deb
 

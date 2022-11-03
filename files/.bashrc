46f00b04e5af191c4bd2e1dfb6dc27bc2ac87cc3
acr <andre.rodrigues.dev@gmail.com>
2022-11-03 04:32:36 +0000

    a

--- files/.bashrc
+++ files/.bashrc
@@ -32,11 +32,6 @@ if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
     debian_chroot=$(cat /etc/debian_chroot)
 fi
 
-# set a fancy prompt (non-color, unless we know we "want" color)
-case "$TERM" in
-    xterm-color|*-256color) color_prompt=yes;;
-esac
-
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 
 if [ -x /usr/bin/dircolors ]; then
@@ -49,9 +44,6 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto'
 fi
 
-# colored GCC warnings and errors
-export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-
 if [ -f ~/.bash_aliases ]; then
     . ~/.bash_aliases
 fi
@@ -63,4 +55,3 @@ if ! shopt -oq posix; then
     . /etc/bash_completion
   fi
 fi
-
--- setup.sh
+++ setup.sh
@@ -3,11 +3,7 @@
 # SYSTEM CONFIG
 
 # install packages
-sudo apt install -y git stow tmux curl htop tree build-essential vlc neovim preload gimp gnome-tweaks qbittorrent
-sudo snap install --classic intellij-idea-community android-studio code
-sudo snap install --classic android-studio 
-sudo snap install --classic code
-sudo snap install --beta steam 
+sudo apt install -y git stow tmux curl htop tree build-essential vlc neovim preload gimp gnome-tweaks transmission 
 sudo snap remove firefox
 
 # setup docker 
@@ -22,9 +18,17 @@ echo \
  curl -L --output-dir /tmp \
 	 -o discord.deb "https://discord.com/api/download?platform=linux&format=deb" \
 	 -o docker.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.13.1-amd64.deb" \
-	 -o chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+	 -o chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" \
+ 	 -o lutris.deb "https://github.com/lutris/lutris/releases/download/v0.5.12-beta1/lutris_0.5.12_beta1_all.deb" \
+ 	 -o steam.deb "https://cdn.akamai.steamstatic.com/client/installer/steam.deb" \
+ 	 -o code.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" \
+ 	 -o jetbrains.tar.gz "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.26.4.13374.tar.gz"
 sudo apt install -y /tmp/*.deb
-
+tar xf /tmp/jetbrains.tar.gz -C /tmp
+/tmp/jetbrains/jetbrains-toolbox
+sudo apt remove --purge --assume-yes snapd
+rm -rf ~/snap/
+sudo rm -rf /var/cache/snapd/ 
 
 # setup dotfiles
 rm ~/.{bashrc,profile,bash_logout}
